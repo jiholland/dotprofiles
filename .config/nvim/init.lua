7ccfcb935bd602efaf9b85bbb6534721e8b640a9
jornivarholland <jornivarholland@icloud.com>
2023-08-04 07:50:00 +0200

    lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,6 @@
 require("config.keymap")
 require("config.option")
 require("config.plugin")
-require("config.lsp")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function() vim.highlight.on_yank() end,
