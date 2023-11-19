f65218024a541d3a06883b988f504621611dffd1
jornivarholland <jornivarholland@icloud.com>
2023-11-19 20:54:50 +0100

    plugins-test

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -6,11 +6,10 @@ return {
     require("lualine").setup({
       options = {
         icons_enabled = true, -- Requires nerd-font.
-        theme = "auto",
         section_separators = "",
         component_separators = "",
       }
     })
-  vim.opt.showmode = false           -- Hide mode indicator.
+  vim.opt.showmode = false  -- Hide mode indicator.
   end,
 }
