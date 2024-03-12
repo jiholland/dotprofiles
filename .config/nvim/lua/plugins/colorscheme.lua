5313f07830e0b5f5aec9bfe09a80314467d8d1b9
jornivarholland <jornivarholland@icloud.com>
2024-03-12 08:46:47 +0100

    fix: colorscheme background

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,9 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      transparent_backgroud = true
+    })
     vim.cmd.colorscheme "catppuccin"
   end,
 }
