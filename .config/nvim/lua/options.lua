90900f61160816671af4256c11e077b216cea027
jornivarholland <jornivarholland@icloud.com>
2025-12-12 20:28:06 +0100

    fix: no local and add completeopt

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,34 +1,32 @@
 -- ~/.config/nvim/lua/options.lua
 
-local g = vim.g
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-g.mapleader = " "
-g.maplocalleader = " "
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
 
-g.loaded_node_provider = 0
-g.loaded_perl_provider = 0
-g.loaded_python3_provider = 0
-g.loaded_ruby_provider = 0
-
-local opt = vim.opt
-
-opt.background = none
-opt.confirm = true
-opt.expandtab = true
-opt.guicursor = ""
-opt.ignorecase = true
-opt.laststatus = 1
-opt.mouse = ""
-opt.number = true
-opt.relativenumber = true
-opt.scrolloff = 5
-opt.shiftwidth = 2
-opt.signcolumn = "yes"
-opt.smartcase = true
-opt.softtabstop = 2
-opt.tabstop = 2
-opt.termguicolors = true
-opt.timeoutlen = 300
-opt.undofile = true
-opt.updatetime = 200
-opt.wrap = false
+vim.opt.background = none
+vim.opt.completeopt = "menu,fuzzy"
+vim.opt.confirm = true
+vim.opt.expandtab = true
+vim.opt.guicursor = ""
+vim.opt.ignorecase = true
+vim.opt.laststatus = 1
+vim.opt.mouse = ""
+vim.opt.number = true
+vim.opt.pumheight = 10
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 5
+vim.opt.shiftwidth = 2
+vim.opt.signcolumn = "yes"
+vim.opt.smartcase = true
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true
+vim.opt.updatetime = 200
+vim.opt.wrap = false
