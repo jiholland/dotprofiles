11567c9d8c63002eeb5d44130f50ff5c8f5f0f39
jornivarholland <jornivarholland@icloud.com>
2025-12-12 20:28:56 +0100

    fix: remove treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,6 +1,7 @@
 -- ~/.config/nvim/lua/plugins.lua
 
 vim.pack.add({
+  "https://github.com/pearofducks/ansible-vim.git",
   "https://github.com/akinsho/bufferline.nvim.git",
   "https://github.com/catppuccin/nvim.git",
   "https://github.com/folke/which-key.nvim.git",
@@ -9,18 +10,10 @@ vim.pack.add({
   "https://github.com/lukas-reineke/indent-blankline.nvim.git",
   "https://github.com/mbbill/undotree.git",
   "https://github.com/nvim-tree/nvim-web-devicons.git",
-  "https://github.com/nvim-treesitter/nvim-treesitter.git",
-  "https://github.com/pearofducks/ansible-vim.git",
 })
 
 require("bufferline").setup({})
 require("ibl").setup({})
-
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "bash", "lua", "python" },
-  highlight = { enable = true },
-  auto_install = true,
-})
 
 require("catppuccin").setup({
   transparent_background = true,
