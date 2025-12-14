d112f86fec4baa4881a6950de87972803f8d5d27
jornivarholland <jornivarholland@icloud.com>
2025-12-14 20:34:13 +0100

    fix: remove plugins

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -13,18 +13,13 @@ map("n", "<leader>dd", vim.diagnostic.open_float, { desc = "Diagnostics: Show cu
 map("n", "<leader>dn", vim.diagnostic.goto_next, { desc = "Diagnostics: Go to next" })
 map("n", "<leader>dp", vim.diagnostic.goto_prev, { desc = "Diagnostics: Go to previous" })
 
+map("n", "<leader>bb", vim.cmd.buffers, { desc = "Buffer: List all buffers" })
 map("n", "<leader>bd", vim.cmd.bdelete, { desc = "Buffer: Close current" })
 map("n", "<leader>bn", vim.cmd.bnext, { desc = "Buffer: Go to next" })
 map("n", "<leader>bp", vim.cmd.bprevious, { desc = "Buffer: Go to previous" })
-map("n", "<leader>bo", function() vim.cmd.BufferLineCloseOthers() end, { desc = "Buffer: Close others" })
 
 map("n", "<leader>fb", function() require("fzf-lua").buffers() end, { desc = "Fuzzy finder: List buffers" })
 map("n", "<leader>fd", function() require("fzf-lua").diagnostics_document() end, { desc = "Fuzzy finder: Diagnostics document" })
 map("n", "<leader>ff", function() require("fzf-lua").files() end, { desc = "Fuzzy finder: Find giles" })
 map("n", "<leader>fg", function() require("fzf-lua").live_grep() end, { desc = "Fuzzy finder: Live grep" })
-
-map("n", "<leader>hp", function() require("gitsigns").preview_hunk_inline() end, { desc = "Git: Preview hunk inline" })
-map("n", "<leader>hs", function() require("gitsigns").stage_hunk() end, { desc = "Git: Stage hunk" })
-map("n", "<leader>hu", function() require("gitsigns").undo_stage_hunk() end, { desc = "Git: Undo stage hunk" })
-
-map("n", "<leader>u", function() vim.cmd.UndotreeToggle() vim.cmd.UndotreeFocus() end, { desc = "Undotree: Toggle and focus" })
+map("n", "<leader>fu", function() require("fzf-lua").undotree() end, { desc = "Fuzzy finder: Undotree" })
