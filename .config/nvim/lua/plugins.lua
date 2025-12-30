e2c36a5dde8780d57e44d5f184f0e8aaecfbd326
jornivarholland <jornivarholland@icloud.com>
2025-12-30 08:45:36 +0100

    chore: name colorscheme plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,10 +1,13 @@
 -- ~/.config/nvim/lua/plugins.lua
 
 vim.pack.add({
-  "https://github.com/catppuccin/nvim.git",
   "https://github.com/ibhagwan/fzf-lua.git",
   "https://github.com/lewis6991/gitsigns.nvim.git",
   "https://github.com/pearofducks/ansible-vim.git",
+  {
+    src = "https://github.com/catppuccin/nvim.git",
+    name = "catppuccin",
+  },
 })
 
 require("catppuccin").setup({
